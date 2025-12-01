6fc5e45c78dcd0163d4efe07dd58c80b493e678e
MillyGit01 <mario.alonsocandela@gmail.com>
2025-12-01 21:24:23 +0100

    Add action button bars for masters

--- gui/BrakeDiscInspector_GUI_ROI/MainWindow.xaml.cs
+++ gui/BrakeDiscInspector_GUI_ROI/MainWindow.xaml.cs
@@ -13047,6 +13047,14 @@ namespace BrakeDiscInspector_GUI_ROI
         private void BtnM1S_Save_Click  (object sender, RoutedEventArgs e) => SaveFor(MasterState.DrawM1_Search);
         private void BtnM1S_Remove_Click(object sender, RoutedEventArgs e) => RemoveFor(MasterState.DrawM1_Search);
 
+        private void BtnM2_Create_Click(object sender, RoutedEventArgs e) => StartDrawingFor(MasterState.DrawM2_Pattern, ComboM2Shape);
+        private void BtnM2_Save_Click  (object sender, RoutedEventArgs e) => SaveFor(MasterState.DrawM2_Pattern);
+        private void BtnM2_Remove_Click(object sender, RoutedEventArgs e) => RemoveFor(MasterState.DrawM2_Pattern);
+
+        private void BtnM2S_Create_Click(object sender, RoutedEventArgs e) => StartDrawingFor(MasterState.DrawM2_Search, ComboM2Shape);
+        private void BtnM2S_Save_Click  (object sender, RoutedEventArgs e) => SaveFor(MasterState.DrawM2_Search);
+        private void BtnM2S_Remove_Click(object sender, RoutedEventArgs e) => RemoveFor(MasterState.DrawM2_Search);
+
         private void BtnEditM2_Click(object sender, RoutedEventArgs e)
         {
             if (_layout == null)
